37a1715bd39e9503da16ffa27127e6ea7e9f94b5
Pedro E. M. Brito <pedroembrito@gmail.com>
2015-02-15 14:13:11 -0200

    Force unused input bytes to '0'

--- siphash.vhd
+++ siphash.vhd
@@ -44,7 +44,13 @@ begin
 
   total_bytes <= std_logic_vector(to_unsigned(block_counter, 5)) & b(2 downto 0);
 
-  this_m <= m when b(3) = '1' else total_bytes & m(55 downto 0);
+  process(m, b, total_bytes)
+  begin
+    this_m               <= (others => '0');
+    this_m(63 downto 56) <= total_bytes;
+    this_m(8*to_integer(unsigned(b))-1 downto 0)
+      <= m(8*to_integer(unsigned(b))-1 downto 0);
+  end process;
 
   process(rst_n, clk)
   begin
